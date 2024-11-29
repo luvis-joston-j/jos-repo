e1bd6d9144d1d612aeaadef1750a58e5eea0f2b9
luvis joston j <luvisjoston@gmail.com>
2024-11-29 21:55:16 +0530

    Update terraform files.tf

--- terraform files.tf
+++ terraform files.tf
@@ -58,3 +58,28 @@ variable "env" {
  default = "Dev"
 }
 ####################################################################################
+provider "aws" {
+  region = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+variable "sec" {
+type = list(number)
+default = [1234,67,89,2233,4444,8080,9090]
+}
+
+resource "aws_security_group" "assign1"{
+   name = "assign1"
+   dynamic "ingress" {
+   for_each = var.sec
+     iterator = port
+    content{
+    from_port        = port.value
+    to_port          = port.value
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+          }
+      }
+}
+#################################################################################################
