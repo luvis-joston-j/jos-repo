b36dec49042fa532c280d774c60f3f16c70599fa
luvis joston j <luvisjoston@gmail.com>
2024-11-29 14:57:35 +0530

    Update terraform files.tf

--- terraform files.tf
+++ terraform files.tf
@@ -3,3 +3,17 @@ provider "aws" {
   access_key = ""
   secret_key = ""
 }
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKI"
+  secret_key = "MJ+"
+}
+
+resource "aws_instance" "myubu" {
+  ami        = "ami-0866a3c8686eaeeba"
+  instance_type = "t2.micro"
+  count = 2
+  tags = {
+    Name = "terraform2"
+  }
+}
